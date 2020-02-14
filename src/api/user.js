cdbf4429bd797c432bf733bd7accee0abf25b40c
wangsidandan <wangsidandan@gmail.com>
2020-02-14 22:57:03 +0800

    初始化项目

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,27 @@
 import request from '@/utils/request'
-
+import querystring from 'querystring'
 export function login(data) {
   return request({
-    url: '/user/login',
+    url: '/login',
     method: 'post',
-    data
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: querystring.stringify(data)
   })
 }
 
 export function getInfo(token) {
   return request({
     url: '/user/info',
-    method: 'get',
-    params: { token }
+    method: 'get'
+    // params: { token }
   })
 }
 
 export function logout() {
   return request({
-    url: '/user/logout',
-    method: 'post'
+    url: '/sign-gout',
+    method: 'get'
   })
 }
--- src/pages/customer/List.vue
+++ src/pages/customer/List.vue
@@ -1,33 +1,33 @@
-  <template>
-  <el-table :data="categoryList" style="width: 100%">
-    <el-table-column prop="id" label="编号" width="180"></el-table-column>
-    <el-table-column prop="username" label="昵称"></el-table-column>
-    <el-table-column prop="realname" label="姓名"></el-table-column>
-    <el-table-column prop="password" label="密码"></el-table-column>
-    <el-table-column prop="enabled" label="状态"></el-table-column>
-    <el-table-column prop="type" label="类型"></el-table-column>
-    <el-table-column prop="telephone" label="电话"></el-table-column>
-  </el-table>
-</template>
-<script>
-import request from "@/utils/request";
-import qs from "querystring";
-export default {
-  data() {
-    return {
-      categoryList:[]
-    };
-  },
-  created() {
-    this.loadUsers();
-  },
-  methods: {
-    loadUsers() {
-      let url="http://134.175.154.93:6677/customer/findAll"
-      request.get(url).then((result)=>{
-          this.categoryList=result.data;
-      })
-    }
-  }
-}
-</script>
+//   <template>
+//   <el-table :data="categoryList" style="width: 100%">
+//     <el-table-column prop="id" label="编号" width="180"></el-table-column>
+//     <el-table-column prop="username" label="昵称"></el-table-column>
+//     <el-table-column prop="realname" label="姓名"></el-table-column>
+//     <el-table-column prop="password" label="密码"></el-table-column>
+//     <el-table-column prop="enabled" label="状态"></el-table-column>
+//     <el-table-column prop="type" label="类型"></el-table-column>
+//     <el-table-column prop="telephone" label="电话"></el-table-column>
+//   </el-table>
+// </template>
+// <script>
+// import request from "@/utils/request";
+// import qs from "querystring";
+// export default {
+//   data() {
+//     return {
+//       categoryList:[]
+//     };
+//   },
+//   created() {
+//     this.loadUsers();
+//   },
+//   methods: {
+//     loadUsers() {
+//       let url="http://134.175.154.93:6677/customer/findAll"
+//       request.get(url).then((result)=>{
+//           this.categoryList=result.data;
+//       })
+//     }
+//   }
+// }
+// </script>
